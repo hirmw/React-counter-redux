ad2fbaa6a18632b761ac6967cf9fe6ba340f8c7a
hirmw <rich.woodhouse@outlook.com>
2020-04-13 11:56:50 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore, combineReducers  } from 'redux'
+
+
+//ACTION -> OBJECT WITH NAME/TYPE
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+
+//REDUCER ^ ACTION -> STORE/STATE
+
+let initialstate = { 
+  result: [{Rock: ['Draw', 'Lost', 'Win'], Scissor: ['Win', 'Drawn', 'Loss'],Paper: ['Loss', 'Win', 'Draw']}]
+};
+
+const counter = (state = [], action) => {
+  switch(action.type){
+    case "INCREMENT":
+      return initialstate;
+    default:
+      return state;
+  }
+};
+
+
+//STORE
+const store = createStore(
+  counter
+);
+
+//DISPLAY
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH -> REDUCER
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +54,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
